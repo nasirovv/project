fe69dc26a5c8dbc3ceb4a8dfe9a2de73c003b70b
nasirovv <69775448+nasirovv@users.noreply.github.com>
2020-08-17 06:33:53 +0500

    new files

---
+++ main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+	$('.slider__content').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false
+	});
+
+	$('news__inner-slider').slick({
+		// nextArrow: '<button type="button" class="slick__news-btn news-next slick-next"></button>',
+		// prevArrow: '<button type="button" class="slick__news-btn news-prev slick-prev"></button>',
+		// infinite: false
+	});
+
+
+	$('.menu__btn').on('click', function(){
+		$('.menu ul').slideToggle();
+	});
+
+});
